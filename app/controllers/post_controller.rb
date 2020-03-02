26fc56305197e80c401d3ae4e63c892bf93f576a
b6eful <phoenixrabang@gmail.com>
2020-03-02 17:05:45 -0600

    worked on controllers

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,22 @@
+class PostController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def delete
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
   
+  get 'comment/show'
+  get 'comment/new'
+  get 'comment/create'
+  get 'comment/delete'
+  get 'post/index'
+  get 'post/show'
+  get 'post/create'
+  get 'post/edit'
+  get 'post/new'
+  get 'post/delete'
+  get 'post/update'
+  get 'user/index'
+  get 'user/show'
+  get 'user/create'
+  get 'user/edit'
+  get 'user/new'
+  get 'user/delete'
+  get 'user/update'
+  get 'user/index'
 end
