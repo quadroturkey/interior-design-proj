74ebce625535f65dc611406f2535fc603f71ff8b
qudroturkey <reesemb1@gmail.com>
2020-03-04 11:16:52 -0600

    main page has multiple posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :posts, dependent: :delete_all
     has_many :items
     has_many :comments, through: :posts
     has_many :likes, through: :posts
-    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'welcome', to: 'sessions#welcome'
-  get 'authorized', to: 'sessions#page_requires_login'    #maybe?
+  get 'authorized', to: 'sessions#page_requires_login'
 end
