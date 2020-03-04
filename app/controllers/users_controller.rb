7c409acb57cc6badd6a02fc365f9f9d0559f271d
qudroturkey <reesemb1@gmail.com>
2020-03-04 16:31:25 -0600

    links to profile posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by(id: session[:user_id])
+    @user = User.find_by(id: params[:id])
     @posts = @user.posts
   end
 
