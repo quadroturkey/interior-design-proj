9bf4543c0b9ab6285f1dcd2c44afeede41cd81ba
qudroturkey <reesemb1@gmail.com>
2020-03-05 14:11:37 -0600

    sort posts by most likes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.sort_by{|post| post.likes.count}.reverse
   end
 
   def show
