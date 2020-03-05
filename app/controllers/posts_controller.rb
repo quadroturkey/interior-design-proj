f3aae794aedcbc2fabe5ffb872c19328256be5f8
Reese Bowman <reesemb1@gmail.com>
2020-03-05 12:21:01 -0600

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,7 +15,7 @@ class PostsController < ApplicationController
   def create
     @user = User.find(session[:user_id])
     @post = Post.new(title: params[:post][:title], description: params[:post][:description], user: current_user)
-    @post.pictures.attach(params[:post][:pictures])
+    @post.picture.attach(params[:post][:picture])
     if @post.valid?
       @post.save
       redirect_to post_path(@post)
