fbcd7638237455e054b8f7e0dcb62239d1f4f11e
qudroturkey <reesemb1@gmail.com>
2020-03-06 10:29:20 -0600

    analytics page

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,21 @@ class Post < ApplicationRecord
     has_many :likes, dependent: :destroy
     has_one_attached :picture
     has_many :comments
+
+
+    def self.number_of_posts
+        self.all.count
+    end
+
+    def self.most_liked_post
+        l_post = nil
+        l_count = 0
+        self.all.each do |post|
+            if post.likes.count > l_count
+                l_post = post
+                l_count = post.likes.count
+            end
+        end
+        l_post
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,36 @@ class User < ApplicationRecord
     has_many :comments, through: :posts
     has_many :likes, dependent: :destroy
     validates :username, uniqueness: true 
+
+    def self.total_users
+        User.count
+    end
+
+    def self.most_posts
+        u = nil
+        i = 0 
+        self.all.each do |user|
+            if user.posts.count >= i
+                u = user
+                i = user.posts.count
+            end
+        end
+        u
+    end
+
+    def self.most_likes
+        u = nil
+        i = 0 
+        self.all.each do |user|
+            if user.likes.count >= i
+                u = user
+                i = user.likes.count
+            end
+        end
+        u
+    end
+
+    def self.lucky
+        self.all[rand(self.all.count)]
+    end
 end
